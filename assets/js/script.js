5e64f8008cdd8793cc9ff55582ec9ac2893afcc9
inqueblot <notminebydesign@gmail.com>
2021-01-13 19:49:16 -0600

    added openweather api, set to search by city

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,19 @@
 $(document).ready(function () {
-    console.log("hello world")
+    // console.log("hello world")
 
+    $("#submitBtn").on("click", function (event) {
+        event.preventDefault();
+        var cityName = $("#cityName").val().trim();
+        queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&APPID=d851f45e5118d3cc096ba04daa669f4a"
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response)
+
+        });
+
+    })
 
 });
