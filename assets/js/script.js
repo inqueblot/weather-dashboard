7736ffcaad7788c8c1cd839c39803589897f850b
inqueblot <notminebydesign@gmail.com>
2021-01-14 00:06:23 -0600

    collect JS variables and create JS logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,17 +3,48 @@ $(document).ready(function () {
 
     $("#submitBtn").on("click", function (event) {
         event.preventDefault();
-        var cityName = $("#cityName").val().trim();
-        queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&APPID=d851f45e5118d3cc096ba04daa669f4a"
+        var searchCity = $("#cityName").val().trim();
+        queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&APPID=d851f45e5118d3cc096ba04daa669f4a&units=imperial"
 
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function (response) {
-            console.log(response)
+            var cityName = response.name;
+            var cityTemp = response.main.temp;
+            var cityHumid = response.main.humidity;
+            var cityWind = response.wind.speed.toFixed(1);
+            var cityLat = response.coord.lat;
+            var cityLon = response.coord.lon;
+
+            querxURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + cityLat + "&lon=" + cityLon + "&appid=d851f45e5118d3cc096ba04daa669f4a&units=imperial"
+
+            $.ajax({
+                url: querxURL,
+                method: "GET"
+            }).then(function (regards) {
+                var uvI = regards.current.uvi
+                var futureTemp = regards.daily[0].temp.day;
+                var futureHumidity = regards.daily[0].humidity;
+                var futureIcon = regards.daily[0].weather[0].icon;
+
+                for (let i = 0; i < 5; i++) {
+                    var div = $("div");
+                    div.text(boogers)
+
+                }
+
+            }
+
+                console.log(regards);
+
+
 
         });
 
-    })
+
+    });
+
+})
 
 });
